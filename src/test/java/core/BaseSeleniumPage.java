ad113e98660cdfb68c76e5589322a9db7087c4a5
smuzhanovskii <kappa@test.com>
2022-06-28 15:19:40 +0200

    Added BaseSeleniumPage, code refactor

--- src/test/java/core/BaseSeleniumPage.java
+++ src/test/java/core/BaseSeleniumPage.java
@@ -1,7 +1,6 @@
 package core;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class BaseSeleniumPage {
     protected static WebDriver driver;
--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,15 +1,11 @@
 package pages;
 
 import core.BaseSeleniumPage;
-import org.apache.commons.logging.Log;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class MainPage extends BaseSeleniumPage {
-//    private FileUploadPage fileUploadPage;
-//    private LoginPage loginPage;
 
     @FindBy(linkText = "File Upload")
     private WebElement fileUpdateLink;
@@ -18,9 +14,6 @@ public class MainPage extends BaseSeleniumPage {
     private WebElement formAuthentication;
 
     public MainPage() {
-        //fileUploadPage = new FileUploadPage(driver);
-        //loginPage = new LoginPage(driver);
-
         PageFactory.initElements(driver,this);
     }
 
